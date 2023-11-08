44299ccbb10520f1912c12dbb618d97792a0983a
unknown <megor7496@gmail.com>
2023-11-08 13:24:26 +0100

    first commit

---
+++ II_TPI_01/assets/app.js
@@ -0,0 +1,42 @@
+//alert("prvy alert Lubos Sukup");
+
+//let prvaPremenna = 3;
+//let druhaPremenna = 7;
+//let vysledok = prvaPremenna + druhaPremenna;
+//alert
+//alert("3 + 7");
+//alert
+//alert(3 + 7);
+//alert
+//alert("prvaPremenna + druhaPremenna");
+//alert
+//alert(prvaPremenna + druhaPremenna);
+//alert
+//alert("vysledok01");
+//alert
+//alert(vysledok01);
+
+//let x = 15;
+//let y = 3;
+//let z = 4;
+//let i = 8 / 2;
+//let vysledok01 = x + y * (z - i);
+//alert(vysledok01);
+
+//let a = 15 + 3 * (4 - 8 / 2);
+//let b = ((150.2 / 8) * (18, 3 - 20.1)) / 3;
+//let c = 3 ** 2 + 8 - 31 / 8;
+
+//let moj vek = 16;
+//let moje meno = "Yehor";
+//let moja adresa = "Rodinie domky 1";
+//let adresa Skoly = "Ńamestye svateho Martina";
+
+let meno = "Yehor";
+let priezvisko = "Moskalenko";
+let mojeMeno = meno + " " + priezvisko;
+
+alert(mojeMeno);
+
+let mojaAdresa = "Ulica test jedna 49";
+let adresaSkoly = "Namestie Svateho Martina";
